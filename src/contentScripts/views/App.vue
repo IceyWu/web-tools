9dc287d4f1250ceceac59d2a9a801d29eed5b8c7
IceyWu <3128006406@qq.com>
2023-12-19 18:27:50 +0800

    feat: add slider com

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -13,29 +13,14 @@ const [show, toggle] = useToggle(false);
 </script>
 
 <template>
-  <div
+  <Slider v-if="show" />
+  <button
+    ref="el"
     :style="style"
-    class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em"
+    class="flex w-15 h-15 rounded-full shadow cursor-pointer border-none fixed z-9999"
+    bg="teal-600 hover:teal-700"
+    @click="toggle()"
   >
-    <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
-      p="x-4 y-2"
-      m="y-auto r-2"
-      transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'"
-    >
-      <h1 class="text-lg">111Vitesse WebExt {{ x }}-{{ y }}-{{ style }}</h1>
-      <SharedSubtitle />
-    </div>
-
-    <button
-      ref="el"
-      :style="style"
-      class="flex w-15 h-15 rounded-full shadow cursor-pointer border-none fixed z-9999"
-      bg="teal-600 hover:teal-700"
-      @click="toggle()"
-    >
-      <pixelarticons-power class="block m-auto text-white text-xl" />
-    </button>
-  </div>
+    <pixelarticons-power class="block m-auto text-white text-xl" />
+  </button>
 </template>
